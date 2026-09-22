41b993d5d8aaeadb7b1f2382110b7f49ca8e5104
jaddunjohn.proclamation@gmail.com <Jaddun>
2026-09-23 02:11:44 +0500

    Add contact page

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact - DevOps Practice</title>
+</head>
+<body>
+
+    <h1>Contact Us</h1>
+    <p>Email: contact@example.com</p>
+    <p>Phone: +92 300 1234567</p>
+
+</body>
+</html>
