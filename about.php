ddb3fc47c13cfc9a0b71b7ecb03fbdf6ff2964c4
jaddunjohn.proclamation@gmail.com <Jaddun>
2026-09-23 02:04:18 +0500

    Add about page

---
+++ about.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About - DevOps Practice</title>
+</head>
+<body>
+
+    <h1>About This Project</h1>
+    <p>This website is created to practice Git and DevOps.</p>
+
+</body>
+</html>
