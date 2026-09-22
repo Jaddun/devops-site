75488a704c14097686191963b7a82690caf794b4
jaddunjohn.proclamation@gmail.com <Jaddun>
2026-09-23 01:56:07 +0500

    Add initial PHP website

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DevOps Practice</title>
+</head>
+<body>
+
+    <h1>Welcome to My DevOps Project</h1>
+    <p>I am learning Git, GitHub and DevOps.</p>
+
+</body>
+</html>
